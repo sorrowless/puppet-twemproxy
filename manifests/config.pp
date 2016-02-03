1d2cd2b6805b0074f606e28ed56c1f1bc6517d25
Daniel Schmitz <daniel@bashgeek.net>
2016-02-03 19:20:33 +0800

    Corrected quotes

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 class twemproxy::config inherits twemproxy {
 
   if $twemproxy::package_manage {
-    file { '/etc/default/${twemproxy::package_name}':
+    file { "/etc/default/${twemproxy::package_name}":
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
